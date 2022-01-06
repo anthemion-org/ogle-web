80ea47983beaad1628cbbb809eec040364c4ed2b
Jeremy Kelly <support@anthemion.org>
2022-01-06 17:01:44 -0600

    Revise CRA setup

---
+++ src/ReportPerform.js
@@ -0,0 +1,22 @@
+// ReportPerform.js
+// ----------------
+// Copyright ©2022 Jeremy Kelly
+// www.anthemion.org
+
+// This was added by CRA. For more on this:
+//
+//   https://create-react-app.dev/docs/measuring-performance/
+//
+const ReportPerform = onPerfEntry => {
+	if (onPerfEntry && onPerfEntry instanceof Function) {
+		import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+			getCLS(onPerfEntry);
+			getFID(onPerfEntry);
+			getFCP(onPerfEntry);
+			getLCP(onPerfEntry);
+			getTTFB(onPerfEntry);
+		});
+	}
+};
+
+export default ReportPerform;
---
+++ src/SetupTest.js
@@ -0,0 +1,11 @@
+// SetupTest.js
+// ------------
+// Copyright ©2022 Jeremy Kelly
+// www.anthemion.org
+
+// This was added by CRA; it adds DOM-checking assertions to Jest. For more on
+// this:
+//
+//   https://github.com/testing-library/jest-dom
+//
+import "@testing-library/jest-dom";
