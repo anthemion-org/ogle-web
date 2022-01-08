49fca5eb25e0cd3cf265310a978e41f323ab841b
Jeremy Kelly <support@anthemion.org>
2022-01-08 17:23:26 -0600

    Create Board class

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Ogle: Word-finding game for the web
 
+## Conventions
+
+Identifier naming conventions [TO DO]
+Origin in lower-left corner [TO DO]
+
+
 ## Project structure
 
 The project was created with _create-react-app_.
---
+++ src/Board/Board.js
@@ -0,0 +1,38 @@
+// Board.js
+// --------
+// Copyright ©2022 Jeremy Kelly
+// www.anthemion.org
+//
+// Import with:
+//
+//   import { tBoard } from "../Board.js";
+//
+
+import { tPool } from "./Pool.js";
+import * as Cfg from "../Cfg.js";
+
+export class tBoard {
+	// Creates a random board.
+	constructor() {
+		// An array of column arrays, which themselves contain tDie instances.
+		this.yDice = [];
+
+		const oqPool = new tPool();
+		for (let oX = 0; oX < Cfg.WthBoard; ++oX) {
+			const oyCol = [];
+			for (let oY = 0; oY < Cfg.HgtBoard; ++oY)
+				oyCol.push(oqPool.uDie())
+			this.yDice.push(oyCol);
+		}
+	}
+
+	// Returns the die at the specified position, throwing if either coordinate is
+	// out of range.
+	uDie(aX, aY) {
+		if ((aX < 0) || (aX >= Cfg.WthBoard))
+			throw new Error("tBoard.uDie: Invalid X position");
+		if ((aY < 0) || (aY >= Cfg.HgtBoard))
+			throw new Error("tBoard.uDie: Invalid Y position");
+		return this.yDice[aX][aY];
+	}
+}
