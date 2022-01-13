4e65722e9733530f6303e2bae6ae1a7f6e52376f
Jeremy Kelly <support@anthemion.org>
2022-01-13 15:35:12 -0600

    Create SearchBoard tests

--- src/Search/SearchBoard.js
+++ src/Search/SearchBoard.js
@@ -5,71 +5,64 @@
 //
 // Import with:
 //
-//   import { tSearchBoard } "./SearchBoard.js";
+//   import * as SearchBoard from "./SearchBoard.js";
 //
 
 import { tLookLex, pOutsLook } from "./LookLex.js";
 import { tSelBoard } from "./SelBoard.js";
 import * as Cfg from "../Cfg.js";
 
-// tSearchBoard
-// ------------
+/** Returns an array of tSelBoard instances representing all words in aqBoard
+ *  that are listed in aqLex, including duplicates and followed words. */
+export function uExec(aqBoard, aqLex) {
+	/** The word selections found during the search, including duplicates and
+	 *  followed words. */
+	const oySelsWord = [];
+	const oyPosi = Cfg.RectBoard.uPosi();
+	for (const oqPos of oyPosi) {
+		const oqSel = new tSelBoard(aqBoard, oqPos);
+		const oqLook = new tLookLex(aqLex, oqSel.TextAll);
+		// At this point, the board selection contains only one die, so it cannot
+		// generate a valid entry. Lookup instances borrow search window state from
+		// their predecessors, however, so starting the search here will save a few
+		// search iterations later:
+		oqLook.uExec(true);
 
-export class tSearchBoard {
-	constructor(aqBoard, aqLex) {
-		/** The board to be searched. */
-		this.qBoard = aqBoard;
-		/** The lexicon to be used while searching. */
-		this.qLex = aqLex;
-		/** The word selections found during the search, including duplicates and
-		 *  followed words. */
-		this.ySelsWord = [];
+		uExecPos(oqSel, oqLook, oySelsWord);
 	}
+	return oySelsWord;
+}
 
-	uExec() {
-		const oyPosi = Cfg.RectBoard.uPosi();
-		for (const oqPos of oyPosi) {
-			const oqSel = new tSelBoard(this.qBoard, oqPos);
-			const oqLook = new tLookLex(this.qLex, oqSel.TextAll);
-			// At this point, the board selection contains only one die, so it cannot
-			// generate a valid entry. Lookup instances borrow search window state from
-			// their predecessors, however, so starting the search here will save a few
-			// search iterations later:
-			oqLook.uExec(true);
+/** Uses the specified tSelBoard amd tLookLex instances to find all word
+ *  selections that begin with aqSel, and adds them to aray aySelsWord. */
+function uExecPos(aqSel, aqLook, aySelsWord) {
+	while (true) {
+		const oqSelNext = aqSel.uNext();
+		// All die sequences following this enumerator have been checked:
+		if (!oqSelNext) return;
 
-			this.uExecPos(oqSel, oqLook);
-		}
-	}
+		const oqLook = tLookLex.suFromPrev(aqLook, oqSelNext.TextAll);
+		const oOutLook = oqLook.uExec(true);
 
-	uExecPos(aqSel, aqLook) {
-		while (true) {
-			const oqSelNext = aqSel.uNext();
-			// All die sequences following this enumerator have been checked:
-			if (!oqSelNext) return;
+		// No words can follow this descendant of aqLook, so continue with the
+		// next descendant:
+		if (oOutLook === pOutsLook.Miss) continue;
 
-			const oqLook = tLookLex.suFromPrev(aqLook, oqSelNext.TextAll);
-			const oOutLook = oqLook.uExec(true);
-
-			// No words can follow this descendant of aqLook, so continue with the
-			// next descendant:
-			if (oOutLook === pOutsLook.Miss) continue;
-
-			if (oqSelNext.TextAll.length >= Cfg.LenWordMin) {
-				if (oOutLook === pOutsLook.Match)
-					this.ySelsWord.push(oqSelNext);
-				else {
-					// A fragment was identified. The search normally stops when this
-					// happens to prevent the search window from narrowing so far that
-					// sequences following the fragment cannot be identified. A fragment
-					// might also be a match, however, so check again without stopping:
-					const oqLookFrag = tLookLex.suFromPrev(aqLook, oqSelNext.TextAll);
-					if (oqLookFrag.uExec(false) == pOutsLook.Match)
-						this.ySelsWord.push(oqSelNext);
-				}
+		if (oqSelNext.TextAll.length >= Cfg.LenWordMin) {
+			if (oOutLook === pOutsLook.Match)
+				aySelsWord.push(oqSelNext);
+			else {
+				// A fragment was identified. The search normally stops when this
+				// happens, to prevent the search window from narrowing so far that
+				// sequences following the fragment cannot be identified. A fragment
+				// might also be a match, however, so check again without stopping:
+				const oqLookFrag = tLookLex.suFromPrev(aqLook, oqSelNext.TextAll);
+				if (oqLookFrag.uExec(false) == pOutsLook.Match)
+					aySelsWord.push(oqSelNext);
 			}
-
-			// Check the descendants of this enumerator:
-			this.uExecPos(oqSelNext, oqLook);
 		}
+
+		// Check the descendants of this selection:
+		uExecPos(oqSelNext, oqLook, aySelsWord);
 	}
 }
--- src/Search/SearchBoard.test.js
+++ src/Search/SearchBoard.test.js
@@ -3,17 +3,69 @@
 // Copyright ©2022 Jeremy Kelly
 // www.anthemion.org
 
-import { tSearchBoard } from "./SearchBoard.js";
+import * as SearchBoard from "./SearchBoard.js";
 import { tBoard } from "../Board/Board.js";
 import { tLex } from "./Lex.js";
 import { tGenRnd } from "../Util/Rnd.js";
 
-test("SearchBoard uExec", () => {
-	const oSeedText = "OGLE";
-	const oqGenRnd = new tGenRnd(oSeedText);
+/** A random number generator for test searches. */
+const SeedTextDef = "OGLE";
+/** The raw word output expected from the first board produced by SeedTextDef.
+ *  These will change if the seed, the random number generator, or the lexicon
+ *  changes. */
+const yWordsExpDef = [
+	"neat", "neon", "neonatal", "neonatal", "neat", "eating", "entangle", "alto",
+	"atonal", "atone", "atone", "atone", "anti", "anting", "anal", "analog",
+	"analogy", "analogs", "aeon", "lanai", "lane", "lane", "lane", "entangle",
+	"eels", "eating", "nans", "nags", "natal", "naming", "namings", "nogs",
+	"note", "noel", "noels", "noting", "neat", "neat", "natal", "tiny", "tins",
+	"ting", "tings", "tingle", "tans", "tansy", "tang", "tangy", "tangs", "tangle", "tango", "tags", "talk", "talks", "tale", "talon", "taming", "togs", "toga",
+	"tole", "tote", "totem", "tonal", "tone", "tone", "tone", "tonal", "mingy",
+	"mingle", "main", "mains", "many", "mans", "mangy", "mangle", "mango", "mags",
+	"magi", "malt", "male", "manta", "mane", "mane", "mane", "mana", "matins",
+	"mating", "matings", "peon", "pels", "pelt", "peen", "peen", "pent", "penal",
+	"penal", "pent", "penal", "penal", "peon", "peel", "peels", "peen", "peen",
+	"peen", "peal", "peat", "elks", "elating", "ogle", "angle", "aglet", "alto",
+	"aloe", "aloe", "alone", "alone", "alone", "anti", "anting", "anal", "atone",
+	"atone", "atone", "atonal", "amigo", "ingot", "ingot", "inane", "inane",
+	"inane", "imago", "melt", "melon", "mental", "mental", "tels", "temp",
+	"tepee", "teen", "teen", "tent", "tenting", "togs", "toga", "tole", "tonal",
+	"tone", "tone", "tone", "tonal", "toting", "total", "totals", "total",
+	"lepton", "lent", "lento", "loan", "loans", "loan", "loam", "logy", "logs",
+	"lone", "lone", "lone", "lain", "lags", "lane", "lane", "lane", "laming",
+	"gnat", "glee", "glen", "gloat", "glans", "glam", "goal", "goals", "goat",
+	"gone", "gone", "gone", "gain", "gains", "gait", "gals", "gale", "galena",
+	"gaol", "gaols", "gamin", "gamins", "gins", "giant", "nags", "natal", "slept",
+	"sleep", "slog", "slogan", "slogan", "slot", "sloe", "sloe", "slot", "slain",
+	"slang", "slangy", "slag", "slant", "slanting", "slat", "slating", "slam",
+	"snag", "snit"
+];
+
+test("SearchBoard uExec: Output", () => {
+	const oqGenRnd = new tGenRnd(SeedTextDef);
 	const oqBoard = new tBoard(oqGenRnd);
 	const oqLex = new tLex;
-	const oqSearch = new tSearchBoard(oqBoard, oqLex);
-	oqSearch.uExec();
-	const oySelsWord = oqSearch.ySelsWord;
+	const oySelsWord = SearchBoard.uExec(oqBoard, oqLex);
+	const oyWords = oySelsWord.map(a => a.TextAll);
+	expect(oyWords).toEqual(yWordsExpDef);
+});
+
+test("SearchBoard uExec: Speed", () => {
+	const oqGenRnd = new tGenRnd(SeedTextDef);
+	const oCt = 20;
+
+	const oTimeStart = Date.now();
+	for (let o = 0; o < oCt; ++o) {
+		const oqBoard = new tBoard(oqGenRnd);
+		const oqLex = new tLex;
+		const oySelsWord = SearchBoard.uExec(oqBoard, oqLex);
+		expect(oySelsWord.length).toBeGreaterThan(10);
+	}
+	const oTimeEnd = Date.now();
+	const oTimePer = (oTimeEnd - oTimeStart) / oCt;
+	console.log(`Search time: ${oTimePer}ms`);
+
+	// This will vary on different machines. On mine, the search takes less than
+	// 13ms:
+	expect(oTimePer).toBeLessThan(20);
 });
