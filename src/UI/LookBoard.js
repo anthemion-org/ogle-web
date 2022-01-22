0edc0ea228eb4d86590a8103787021a49fcb4f5b
Jeremy Kelly <general@anthemion.org>
2022-01-21 18:22:47 -0600

    Move selection handlers to ViewPlay

--- src/UI/LookBoard.js
+++ src/UI/LookBoard.js
@@ -13,7 +13,6 @@ import BackDie from "./BackDie.js";
 import ConnSel from "./ConnSel.js";
 import LookDie from "./LookDie.js";
 import { tBoard } from "../Board/Board.js";
-import { tSelBoard } from "../Board/SelBoard.js";
 import { tPt2 } from "../Util/Pt2.js";
 import * as Cfg from "../Cfg.js";
 
@@ -28,59 +27,26 @@ import PropTypes from "prop-types";
  *
  *  ~ Board: A tBoard instance representing the board to be played. This prop is
  *    required.
+ *
+ *  ~ Sel: A tSelBoard instance representing the board selection, or 'undefined'
+ *    if there is no selection.
+ *
+ *  ~ uCallTog: A function to be invoked when the die is left-clicked. This prop
+ *    is required.
+ *
+ *  ~ uCallClear: A function to be invoked when the die is middle-clicked. This
+ *    prop is required.
+ *
+ *  ~ uCallEnt: A function to be invoked when the die is right-clicked. This
+ *    prop is required.
  */
 export default class LookBoard extends React.Component {
-	constructor(aProps) {
-		super(aProps);
-
-		// Restore from Store: [to do]
-		let oSel = null;
-
-		this.state = {
-			Sel: oSel
-		};
-
-		this.uTog_Die = this.uTog_Die.bind(this);
-		this.uClear_Sel = this.uClear_Sel.bind(this);
-		this.uEnt_Sel = this.uEnt_Sel.bind(this);
-	}
-
-	uTog_Die(aPos) {
-		if (!this.uCkEnab(aPos)) return;
-
-		if (!this.state.Sel) {
-			const oSelNew = new tSelBoard(this.props.Board, aPos);
-			this.setState({ Sel: oSelNew });
-			return;
-		}
-
-		const oSelAt = this.state.Sel.SelsByPos.uGet(aPos);
-		if (oSelAt) {
-			this.setState({ Sel: oSelAt.SelPrev });
-			return;
-		}
-
-		const oSelAdd = new tSelBoard(this.props.Board, aPos, this.state.Sel);
-		this.setState({ Sel: oSelAdd });
-	}
-
-	uClear_Sel() {
-		this.setState({ Sel: null });
-	}
-
-	uEnt_Sel() {
-		this.setState({ Sel: null });
-	}
-
-	componentDidUpdate() {
-	}
-
 	/** Returns 'true' if the specified board position can be selected or
 	 *  unselected. */
 	uCkEnab(aPos) {
-		return !this.state.Sel
-			|| this.state.Sel.uCkAddAt(aPos)
-			|| !!this.state.Sel.SelsByPos.uGet(aPos);
+		return !this.props.Sel
+			|| this.props.Sel.uCkAddAt(aPos)
+			|| !!this.props.Sel.SelsByPos.uGet(aPos);
 	}
 
 	uBacksDie() {
@@ -102,7 +68,7 @@ export default class LookBoard extends React.Component {
 			for (let oY = 0; oY < Cfg.HgtBoard; ++oY) {
 				const oKey = oX + "/" + oY;
 				const oPos = new tPt2(oX, oY);
-				const oSelAt = this.state.Sel && this.state.Sel.uSelAt(oPos);
+				const oSelAt = this.props.Sel && this.props.Sel.uSelAt(oPos);
 				const oPosFrom = oSelAt?.SelPrev?.Pos;
 				oEls.push(
 					<ConnSel key={oKey} Pos={oPos} PosFrom={oPosFrom} />
@@ -118,11 +84,11 @@ export default class LookBoard extends React.Component {
 				const oKey = oX + "/" + oY;
 				const oPos = new tPt2(oX, oY);
 				const oDie = this.props.Board.uDie(oPos);
-				const oSelAt = this.state.Sel && this.state.Sel.uSelAt(oPos);
+				const oSelAt = this.props.Sel && this.props.Sel.uSelAt(oPos);
 				oEls.push(
 					<LookDie key={oKey} Pos={oPos} Die={oDie} CkSel={!!oSelAt}
-						CkEnab={this.uCkEnab(oPos)} uCallTog={this.uTog_Die}
-						uCallClear={this.uClear_Sel} uCallEnt={this.uEnt_Sel} />
+						CkEnab={this.uCkEnab(oPos)} uCallTog={this.props.uCallTog}
+						uCallClear={this.props.uCallClear} uCallEnt={this.props.uCallEnt} />
 				);
 			}
 		return oEls;
--- src/UI/ViewPlay.js
+++ src/UI/ViewPlay.js
@@ -12,6 +12,7 @@ import "./ViewPlay.css";
 import * as StApp from "../StApp.js";
 import { tSetup } from "../Setup.js";
 import { tBoard } from "../Board/Board.js";
+import { tSelBoard } from "../Board/SelBoard.js";
 import LookBoard from "./LookBoard.js";
 import * as ActPlay from "./ActPlay.js";
 
@@ -31,20 +32,27 @@ import PropTypes from "prop-types";
  *    required.
  */
 export default function ViewPlay(aProps) {
+	/** Set to 'true' if the game is paused. */
 	const [oCkPause, ouSet_CkPause] = useState(false);
+	/** The selection within the board, or 'null' if there is no selection. */
+	const [oSel, ouSet_Sel] = useState(null);
 
+	/** Handles the Pause button click. */
 	function ouHandPause(aEvt) {
 		ouSet_CkPause(true);
 	}
 
+	/** Handles the Resume button click. */
 	function ouHandResume(aEvt) {
 		ouSet_CkPause(false);
 	}
 
+	/** Handles the End Round button click. */
 	function ouHandEnd(aEvt) {
 		aProps.uDispatStApp(StApp.Views.Setup);
 	}
 
+	/** Returns the Pause dialog, or 'null' if the game is not paused. */
 	function ouDlgPause() {
 		if (!oCkPause) return null;
 
@@ -58,11 +66,48 @@ export default function ViewPlay(aProps) {
 		);
 	}
 
+	/** Returns 'true' if the specified board position can be selected or
+	 *  unselected. */
+	function ouCkEnab(aPos) {
+		return !oSel || oSel.uCkAddAt(aPos) || !!oSel.SelsByPos.uGet(aPos);
+	}
+
+	/** Toggles the die selection at the specified board position. */
+	function ouTog_Die(aPos) {
+		if (!ouCkEnab(aPos)) return;
+
+		if (!oSel) {
+			const oSelNew = new tSelBoard(aProps.Board, aPos);
+			ouSet_Sel(oSelNew);
+			return;
+		}
+
+		const oSelAt = oSel.SelsByPos.uGet(aPos);
+		if (oSelAt) {
+			ouSet_Sel(oSelAt.SelPrev);
+			return;
+		}
+
+		const oSelAdd = new tSelBoard(aProps.Board, aPos, oSel);
+		ouSet_Sel(oSelAdd);
+	}
+
+	/** Clears the board selection. */
+	function ouClear_Sel() {
+		ouSet_Sel(null);
+	}
+
+	/** Records the board selection as an entry. */
+	function ouEnt_Sel() {
+		ouSet_Sel(null);
+	}
+
 	return (
 		<div id="ViewPlay">
 			<h1>Ogle</h1>
 
-			<LookBoard Board={aProps.Board} />
+			<LookBoard Board={aProps.Board} Sel={oSel} uCallTog={ouTog_Die}
+				uCallClear={ouClear_Sel} uCallEnt={ouEnt_Sel} />
 
 			<div className="Btns">
 				<button onClick={ouHandPause}>Pause</button>
