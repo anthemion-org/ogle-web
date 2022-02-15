c6120ba00fb71ba1434192bf9b0487c5d581cd84
Jeremy Kelly <support@anthemion.org>
2022-02-14 21:30:18 -0600

    Simplify Logo component Improve colors

--- src/UI/Logo.js
+++ src/UI/Logo.js
@@ -8,154 +8,31 @@
 //   import Logo from "./UI/Logo";
 //
 
+import "./Die.css";
+import BackDie from "./BackDie.js";
+import { tPt2 } from "../Util/Pt2.js";
+
 import React from "react";
 
 export default function Logo(aProps) {
+	const oPos = new tPt2(0, 0);
+
 	return (
-		<svg {...aProps}
+		<svg {...aProps} className="Logo"
 			xmlns="http://www.w3.org/2000/svg"
-			width="4in"
-			height="4in"
-			viewBox="0 0 101.6 101.6"
+			viewBox="0 0 100 100"
 		>
-			<defs>
-				<clipPath id="clipPath1164">
-					<rect
-						width="101.6"
-						height="101.6"
-						x="0"
-						y="208.1"
-						fill="#020201"
-						fillRule="evenodd"
-						color="#000"
-						rx="12.709"
-						ry="12.709"
-						style={{ isolation: "auto", mixBlendMode: "normal" }}
-					></rect>
-				</clipPath>
-				<clipPath id="clipPath1168">
-					<rect
-						width="101.6"
-						height="101.6"
-						x="0"
-						y="208.1"
-						fill="#020201"
-						fillRule="evenodd"
-						color="#000"
-						rx="12.709"
-						ry="12.709"
-						style={{ isolation: "auto", mixBlendMode: "normal" }}
-					></rect>
-				</clipPath>
-				<clipPath id="clipPath1172">
-					<rect
-						width="101.6"
-						height="101.6"
-						x="0"
-						y="208.1"
-						fill="#020201"
-						fillRule="evenodd"
-						color="#000"
-						rx="12.709"
-						ry="12.709"
-						style={{ isolation: "auto", mixBlendMode: "normal" }}
-					></rect>
-				</clipPath>
-				<clipPath id="clipPath1176">
-					<rect
-						width="101.6"
-						height="101.6"
-						x="0"
-						y="208.1"
-						fill="#020201"
-						fillRule="evenodd"
-						color="#000"
-						rx="12.709"
-						ry="12.709"
-						style={{ isolation: "auto", mixBlendMode: "normal" }}
-					></rect>
-				</clipPath>
-			</defs>
-			<g fillRule="evenodd" color="#000" transform="translate(0 -208.1)">
-				<rect
-					width="50.8"
-					height="50.8"
-					x="50.8"
-					y="258.9"
-					fill="#c7b9ab"
-					clipPath="url(#clipPath1176)"
-					rx="0"
-					ry="0"
-					style={{ isolation: "auto", mixBlendMode: "normal" }}
-				></rect>
-				<rect
-					width="50.8"
-					height="50.8"
-					x="0"
-					y="258.9"
-					fill="#ebddce"
-					clipPath="url(#clipPath1172)"
-					rx="0"
-					ry="0"
-					style={{ isolation: "auto", mixBlendMode: "normal" }}
-				></rect>
-				<rect
-					width="50.8"
-					height="50.8"
-					x="50.8"
-					y="208.1"
-					fill="#ebddce"
-					clipPath="url(#clipPath1168)"
-					rx="0"
-					ry="0"
-					style={{ isolation: "auto", mixBlendMode: "normal" }}
-				></rect>
-				<rect
-					width="50.8"
-					height="50.8"
-					x="0"
-					y="208.1"
-					fill="#fff"
-					clipPath="url(#clipPath1164)"
-					rx="0"
-					ry="0"
-					style={{ isolation: "auto", mixBlendMode: "normal" }}
-				></rect>
-			</g>
-			<rect
-				width="88.9"
-				height="88.9"
-				fill="#f5efe9"
-				fillRule="evenodd"
-				color="#000"
-				rx="32.241"
-				ry="32.241"
-				transform="scale(1.1429)"
-				style={{ isolation: "auto", mixBlendMode: "normal" }}
-			></rect>
-			<text
-				x="50.8"
-				y="78.298"
-				fill="none"
-				stroke="#000"
-				strokeWidth="0.265"
-				fontFamily="Arial"
-				fontSize="79.375"
-				letterSpacing="0"
-				textAnchor="middle"
-				wordSpacing="0"
-				style={{ lineHeight: "150%" }}
+			<BackDie Pos={oPos} CkDisp={true} />
+
+			<text className="Text"
+				x="50" y="50"
+				fontSize="80px"
+				letterSpacing="0px"
+				stroke="#000000" strokeWidth="0px"
+				textAnchor="middle" dominantBaseline="central"
+				wordSpacing="0px"
 			>
-				<tspan
-					x="50.8"
-					y="78.298"
-					fill="#000"
-					stroke="none"
-					fontFamily="'Brygada 1918'"
-					fontWeight="bold"
-				>
-					O
-				</tspan>
+				O
 			</text>
 		</svg>
 	);
